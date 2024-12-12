4484f26f4456b6dabe30d3f8e065f94dd912f982
AYanovDev <alex.ianovskii@mail.ru>
2024-12-12 20:53:33 +0500

    Advanced the logic

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
 let gameBoard = (() => {
-  let board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
   let boxes = document.querySelectorAll(".box");
   boxes.forEach(function (box) {
-    box.addEventListener("click", function () {
+    function pickEvent() {
       if (player_x.isDrawing) {
         box.textContent = player_x.symbol;
+        player_x.isDrawing = false;
+        player_o.isDrawing = true;
       } else {
         box.textContent = player_o.symbol;
+        player_o.isDrawing = false;
+        player_x.isDrawing = true;
       }
-    });
+      box.removeEventListener("click", pickEvent);
+    }
+    box.addEventListener("click", pickEvent);
   });
-  let row1 = [board[0], board[1], board[2]];
-  let row2 = [board[3], board[4], board[5]];
-  let row3 = [board[6], board[7], board[8]];
-  let column1 = [board[0], board[3], board[6]];
-  let column2 = [board[1], board[4], board[7]];
-  let column3 = [board[2], board[5], board[8]];
-  let cross1 = [board[0], board[4], board[8]];
-  let cross2 = [board[2], board[4], board[6]];
+  let row1 = [boxes[0], boxes[1], boxes[2]];
+  let row2 = [boxes[3], boxes[4], boxes[5]];
+  let row3 = [boxes[6], boxes[7], boxes[8]];
+  let column1 = [boxes[0], boxes[3], boxes[6]];
+  let column2 = [boxes[1], boxes[4], boxes[7]];
+  let column3 = [boxes[2], boxes[5], boxes[8]];
+  let cross1 = [boxes[0], boxes[4], boxes[8]];
+  let cross2 = [boxes[2], boxes[4], boxes[6]];
 })();
 
 function createPlayer(symbol, isDrawing) {
